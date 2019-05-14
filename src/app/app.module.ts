6d7ab5239dbea1e66bd6f86390238f69464273ea
shuyang <shuyang@shuyangdeMBP.fios-router.home>
2019-05-13 21:17:16 -0400

    fix the tar:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { SiderbarComponent } from './siderbar/siderbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    SiderbarComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
